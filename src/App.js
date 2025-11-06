563d9dcc3478a13b336b8dbdd035a9f300d8cea5
Sidiq Moltafet <sidiqmoltafet@gmail.com>
2025-11-05 18:14:10 -0800

    integrate with Statsig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { StatsigProvider, useClientAsyncInit } from '@statsig/react-bindings';
+import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
+import { StatsigSessionReplayPlugin } from '@statsig/session-replay';
 import Hero from './components/Hero';
 import WhyUs from './components/WhyUs';
 import Footer from './components/Footer.jsx';
@@ -39,17 +42,25 @@ function HomePage() {
 }
 
 function App() {
+  const { client } = useClientAsyncInit(
+    'client-F5hI5bm92bzJgiBvuGw65tQ2WUNl5rdbjB5iYiMd6Yk',
+    { userID: 'a-user' },
+    { plugins: [new StatsigAutoCapturePlugin(), new StatsigSessionReplayPlugin()] }
+  );
+
   return (
-    <GoogleOAuthProvider clientId="859607125627-3ejhtjo5mc7gcrvhrr833g2aq5mttq3p.apps.googleusercontent.com">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/schedule" element={<Calendar />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </GoogleOAuthProvider>
+    <StatsigProvider client={client} loadingComponent={<div>Loading...</div>}>
+      <GoogleOAuthProvider clientId="859607125627-3ejhtjo5mc7gcrvhrr833g2aq5mttq3p.apps.googleusercontent.com">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/schedule" element={<Calendar />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </GoogleOAuthProvider>
+    </StatsigProvider>
   );
 }
 
